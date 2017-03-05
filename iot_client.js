f7e90c72759e9b96c78c41e51876aa6a1d0656c7
Nathan Pearce <n.pearce@f5.com>
2017-03-04 16:33:50 -0800

    Incrementing by lastUpdateMicros intervals

--- iot_client.js
+++ iot_client.js
@@ -2,24 +2,20 @@
 var http = require("https");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //ignore self-signed cert
 
-var inputsCount = 1;
 var recordsCount = 1;
 var noInputs = 0;
-
-var inputs = JSON.parse('{ "poll_inputs_interval": "1000" }');  //setting a poll interval for first run
-
-//if ((WATER_MARK - SAMPLE_SIZE) < SAMPLE_SIZE) {
-//  console.log('SAMPLE_SIZE: ' +SAMPLE_SIZE+ ' is greater than SAMPLE_SIZE minus WATER_MARK');
-//}
+var fromLastUpdateMicros = 0;
+var records = [];
+var DEBUG = true;
+var inputs = { "poll_inputs_interval": "1000" };  //setting a poll interval for first run
 
 function updateInputs() {  //Retreives operational settings from git repo
 
-  if ((inputs.alive === "false") || (inputsCount > "5")) {
+  if ((inputs.alive === "false") || (noInputs > "5")) {
     console.log("Kill triggered. Exiting...");
     process.exit();
   }
   else {
-
     var options = {
       "method": "GET",
       "hostname": "raw.githubusercontent.com",
@@ -41,34 +37,35 @@ function updateInputs() {  //Retreives operational settings from git repo
       res.on("end", function () {
         var body = Buffer.concat(chunks);
         inputs = JSON.parse(body);
-        console.log("inputs: " +JSON.stringify(inputs, ' ', '\t'));
+        if (DEBUG == true) { console.log("DEBUG: inputs: " +JSON.stringify(inputs, ' ', '\t')) };
       });
     });
 
     req.end();
 
-    console.log("inputsCount: " +inputsCount);
-    inputsCount++;
-
-  //  console.log("inputs.poll_inputs_interval: " +inputs.poll_inputs_interval);
-    setTimeout(updateInputs, inputs.poll_inputs_interval);
+    if (!inputs.domain_batch_size) {
+      if (DEBUG == true) { console.log("Awaiting inputs. No Inputs count: " +noInputs) };
+      noInputs++;
+      setTimeout(updateInputs, 5000);  // Retry iot_client_inputs.json in 5 seconds.
+    }
+    else {
+      setTimeout(updateInputs, inputs.poll_inputs_interval);
+    }
   }
 }
 
-function processRecords() {   //Get the last 'inputs.domain_batch_size' records since 'lastUpdateMicros'
-  if (!inputs.domain_batch_size) {
-    console.log("Awaiting inputs. No Inputs count: " +noInputs);
-    noInputs++;
-    setTimeout(processRecords, 5000);
-  }
-  else {
-    console.log("getDomains() domain_batch_size: " +inputs.domain_batch_size);
+function processRecords() {   //Collect 'inputs.domain_batch_size' of records until complete.
+  if (inputs.domain_batch_size) {  //If we've acquired the environment settings....
+    if (DEBUG == true) {
+      console.log("Collecting " +inputs.domain_batch_size+ " records.");
+      console.log("fromLastUpdateMicros: " +fromLastUpdateMicros);
+    }
 
     var options = {
       "method": "GET",
       "hostname": "192.168.202.162",
       "port": 443,
-      "path": "/mgmt/demo/domains-journal?$filter=lastUpdateMicros%20gt%20%271487377200000000%27&$select=op,ipAddress,domainName,lastUpdateMicros&$top=" +inputs.domain_batch_size+ "&$orderby=lastUpdateMicros",
+      "path": "/mgmt/demo/domains-journal?$filter=lastUpdateMicros%20gt%20%27"+fromLastUpdateMicros+"%27&$select=op,ipAddress,domainName,lastUpdateMicros&$top="+inputs.domain_batch_size+"&$orderby=lastUpdateMicros",
       "headers": {
         "authorization": "Basic YWRtaW46YWRtaW4=",
         "cache-control": "no-cache"
@@ -84,19 +81,30 @@ function processRecords() {   //Get the last 'inputs.domain_batch_size' records 
 
       res.on("end", function () {
         var body = Buffer.concat(chunks);
-        console.log("You caught a fish this big: " +body.toString().length);
-//        console.log("getDomains() inputs: " +body.toString());
+        jBody = JSON.parse(body);
+//        console.log("Response - jBody " +JSON.stringify(jBody, ' ', '\t')); // the entire resposne.
+        console.log("lastUpdateMicros of item "+inputs.domain_batch_size+" : " +jBody.items[(inputs.domain_batch_size - 1)].lastUpdateMicros);
+
+        fromLastUpdateMicros = jBody.items[(inputs.domain_batch_size - 1)].lastUpdateMicros;  //Grab the lastUpdateMicros of the last entry.
+
+        for (var i in jBody.items)  {
+          console.log("jBody.items[i].domainName: " +jBody.items[i].domainName+ " .ipAddress " +jBody.items[i].ipAddress);
+  //        var name = jBody.items[i].domainName;
+  //        var address = jBody.items[i].ipAddress;
+          var record = jBody.items[i].domainName+" "+jBody.items[i].ipAddress;
+          records.push(record);
+          console.log("records[i]: " +records[i]);
+        }
+        console.log("all the records: " +JSON.stringify(records, ' ', '\t'));  //dump all the records
       });
     });
 
     req.end();
 
-    console.log("processRecords count: " +recordsCount);
-    recordsCount++;
-
-    setTimeout(processRecords, inputs.poll_domains_interval);
   }
+  setTimeout(processRecords, inputs.poll_domains_interval);
 }
+
 function postUpdate(update) {   //POST to the dashboard wWen you are all caught up.
 
   var http = require("https");
